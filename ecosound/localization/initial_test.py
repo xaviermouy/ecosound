5dac054f17e03cf1f98a4319641dfa81141204a2
xaviermouy <xavier.mouy@gmail.com>
2021-05-21 22:21:54 -0700

    uncertainties

--- ecosound/localization/initial_test.py
+++ ecosound/localization/initial_test.py
@@ -71,14 +71,12 @@ def run_detector(infile, channel, config, chunk=None, deployment_file=None):
                          inplace=True,
                          )
     # Denoise
-    print('Denoise')
     spectro.denoise(config['DENOISER']['denoiser_name'],
                     window_duration=config['DENOISER']['window_duration_sec'],
                     use_dask=config['DENOISER']['use_dask'],
                     dask_chunks=tuple(config['DENOISER']['dask_chunks']),
                     inplace=True)
     # Detector
-    print('Detector')
     file_timestamp = ecosound.core.tools.filename_to_datetime(sound.file_full_path)[0]
     detector = DetectorFactory(config['DETECTOR']['detector_name'],
                                kernel_duration=config['DETECTOR']['kernel_duration_sec'],
@@ -145,6 +143,41 @@ def plot_data(audio_files,frame, window_type, nfft, step, fmin, fmax, chunk = No
     graph_spectros.show()
     graph_waveforms.show()
 
+
+def stack_waveforms(audio_files, detec, TDOA_max_sec):
+    waveform_stack = []
+    for audio_file, channel in zip(audio_files['path'], audio_files['channel'] ): # for each channel
+        # load waveform
+        chan_wav = Sound(audio_file)
+        chan_wav.read(channel=channel,
+                      chunk=[detec['time_min_offset']-TDOA_max_sec, detec['time_max_offset']+TDOA_max_sec],
+                      unit='sec',
+                      detrend=True)
+        # bandpass filter
+        chan_wav.filter('bandpass', [detec['frequency_min'], detec['frequency_max']])
+        # stack
+        waveform_stack.append(chan_wav.waveform)
+    return waveform_stack
+
+
+def calc_data_error(tdoa_sec, m, sound_speed_mps,hydrophones_config, hydrophone_pairs):
+    """ Calculates tdoa measurement errors. Eq. (9) in Mouy et al. 2018"""
+    tdoa_m = predict_tdoa(m, sound_speed_mps, hydrophones_config, hydrophone_pairs)
+    Q = len(m)
+    M = m.size
+    N = len(tdoa_sec)
+    error_std = np.sqrt((1/(Q*(N-M))) * (sum((tdoa_sec-tdoa_m)**2)))
+    return error_std
+
+
+def calc_loc_errors(tdoa_errors_std, m, sound_speed_mps, hydrophones_config, hydrophone_pairs):
+    """ Calculates localization errors. Eq. (8) in Mouy et al. 2018."""
+    A = defineJacobian(hydrophones_config, m, sound_speed_mps, hydrophone_pairs)
+    Cm = (tdoa_errors_std**2) * np.linalg.inv(np.dot(A.transpose(),A)) # Model covariance matrix for IID
+    err_std = np.sqrt(np.diag(Cm))
+    return pd.DataFrame({'x_std': err_std[0], 'y_std': err_std[1], 'z_std': err_std[2]})
+
+
 ## ############################################################################
 
 # Config files
@@ -154,7 +187,7 @@ detection_config_file = r'C:\Users\xavier.mouy\Documents\GitHub\ecosound\ecosoun
 localization_config_file = r'C:\Users\xavier.mouy\Documents\GitHub\ecosound\ecosound\localization\config\localization_config.yaml'
 
 # load configuration parameters
-hydrophones_config= pd.read_csv(hydrophones_config_file) # load hydrophone coordinates (meters)
+hydrophones_config= pd.read_csv(hydrophones_config_file, skipinitialspace=True) # load hydrophone coordinates (meters)
 detection_config = read_yaml(detection_config_file)
 localization_config = read_yaml(localization_config_file)
 
@@ -186,9 +219,9 @@ plot_data(audio_files,
           detections_channel=detection_config['AUDIO']['channel'])
 
 
-## ###########################################################################
-##                                   TDOA
-## ###########################################################################
+# localization
+sound_speed_mps = localization_config['ENVIRONMENT']['sound_speed_mps']
+ref_channel = localization_config['TDOA']['ref_channel']
 
 # define search window based on hydrophone separation and sound speed
 hydrophones_dist_matrix = calc_hydrophones_distances(hydrophones_config)
@@ -201,87 +234,53 @@ hydrophone_pairs = defineReceiverPairs(len(hydrophones_config), ref_receiver=ref
 detec = detections.data.iloc[0]
 #detec = detections.data.iloc[33]
 
-# Adjust start/stop times of detection on reference channel
-detec_wav = sound.select_snippet([detec['time_min_offset'],detec['time_max_offset']], unit='sec')
-detec_wav.tighten_waveform_window(95)
-TDOA_max_samp = int(np.round(TDOA_max_sec*sound.waveform_sampling_frequency))
-t1_detec = detec_wav.waveform_start_sample - TDOA_max_samp
-t2_detec = detec_wav.waveform_stop_sample + TDOA_max_samp
-
 # load data from all channels for that detection
-waveform_stack = []
-for idx, audio_file in enumerate(audio_files):  # for each channel
-    # load waveform
-    chan_wav = Sound(audio_file)
-    chan_wav.read(channel=0,
-                  chunk=[t1_detec, t2_detec],
-                  unit='samp',
-                  detrend=True)
-    sampling_frequency = chan_wav.waveform_sampling_frequency
-    # bandpass filter
-    chan_wav.filter('bandpass', [detec['frequency_min'], detec['frequency_max']])
-    # stack
-    waveform_stack.append(chan_wav.waveform)
+waveform_stack = stack_waveforms(audio_files, detec, TDOA_max_sec)
 
 # calculate TDOAs
 tdoa_sec, corr_val = calc_tdoa(waveform_stack,
                                hydrophone_pairs,
-                               sampling_frequency,
+                               detec['audio_sampling_frequency'],
                                TDOA_max_sec=TDOA_max_sec,
-                               upsample_res_sec=0.0000001,
-                               normalize=False,
-                               doplot=False,
+                               upsample_res_sec=localization_config['TDOA']['upsample_res_sec'],
+                               normalize=localization_config['TDOA']['normalize'],
+                               doplot=True,
                                )
 ## TO DO
 # If correlation coef too small =>
 # 1 - calc TDOA on sliding window
 # 2 - calc TDOA on narrow frequency bands
 # 3 - Use less Hp to localize
-## ###########################################################################
-##                                Localisation
-## ###########################################################################
-
-# #TDOA
-# ref_channel=3
-# #Linearized inversion
-# sound_speed_mps = 1484
-# inversion_params = {
-#     'start_model':[0,0,0],
-#     'start_model_repeats': 5,
-#     'damping_factor': 0.2,
-#     'stop_delta_m': 0.01, # threshold for stoping iterations (change in norm of models)
-#     'stop_max_iteration': 1000,
-#     }
 
 # Lineralized inversion
 [m, iterations_logs] = linearized_inversion(tdoa_sec,
                                             hydrophones_config,
                                             hydrophone_pairs,
-                                            inversion_params,
+                                            localization_config['INVERSION'],
                                             sound_speed_mps,
                                             doplot=False)
 
-## TODO: verify that the localization correspond to teh fish/matlab
+# Estimate uncertainty
+tdoa_errors_std = calc_data_error(tdoa_sec, m, sound_speed_mps,hydrophones_config, hydrophone_pairs)
+loc_errors_std = calc_loc_errors(tdoa_errors_std, m, sound_speed_mps, hydrophones_config, hydrophone_pairs)
 
-print('s')
 
-
-# # plot hydrophones
-# fig1 = plt.figure()
-# ax = fig1.add_subplot(111, projection='3d')
-# colors = matplotlib.cm.tab10(hydrophones_config.index.values)
-# # Sources
-# for index, hp in hydrophones_config.iterrows():
-#     point = ax.scatter(hp['x'],hp['y'],hp['z'],
-#                     s=20,
-#                     color=colors[index],
-#                     label=hp['name'],
-#                     )
-# # Axes labels
-# ax.set_xlabel('X (m)', labelpad=10)
-# ax.set_ylabel('Y (m)', labelpad=10)
-# ax.set_zlabel('Z (m)', labelpad=10)
-# # legend
-# ax.legend(bbox_to_anchor=(1.07, 0.7, 0.3, 0.2), loc='upper left')
-# plt.tight_layout()
-# plt.show()
+# Plot hydrophones
+fig1 = plt.figure()
+ax = fig1.add_subplot(111, projection='3d')
+colors = matplotlib.cm.tab10(hydrophones_config.index.values)
+# Sources
+for index, hp in hydrophones_config.iterrows():
+    point = ax.scatter(hp['x'],hp['y'],hp['z'],
+                    s=20,
+                    color=colors[index],
+                    label=hp['name'],
+                    )
+# Axes labels
+ax.set_xlabel('X (m)', labelpad=10)
+ax.set_ylabel('Y (m)', labelpad=10)
+ax.set_zlabel('Z (m)', labelpad=10)
+# legend
+ax.legend(bbox_to_anchor=(1.07, 0.7, 0.3, 0.2), loc='upper left')
+plt.tight_layout()
+plt.show()
