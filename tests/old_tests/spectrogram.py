f61d70f5b429602d93d6fd152bb88b077aa39e3b
Xavier Mouy <xavier.mouy@gmail.com>
2025-04-24 22:21:24 -0400

    added new timestanp format

--- tests/old_tests/spectrogram.py
+++ tests/old_tests/spectrogram.py
@@ -10,23 +10,23 @@ from ecosound.core.audiotools import Sound
 from ecosound.core.spectrogram import Spectrogram
 from ecosound.visualization.grapher_builder import GrapherFactory
 import time
-
+import matplotlib.pyplot as plt
 ## Input paraneters ##########################################################
 
-single_channel_file = r"../ecosound/resources/AMAR173.4.20190916T061248Z.wav"
+single_channel_file = r"C:\Users\xavier.mouy\Documents\Proposals\2025_SeaGrant-WHOI\proposal\drafts\figures\detector\SoundSample_WellfleetHarbor_20240627.wav"
 
 # Spectrogram parameters
 frame = 3000
 nfft = 4096
 step = 500
 #ovlp = 2500
-fmin = 0 
-fmax = 1000
+fmin = 0
+fmax = 10000
 window_type = 'hann'
 
 # start and stop time of wavfile to analyze
-t1 = 1515
-t2 = 1541
+t1 = 1.5
+t2 = 3.6
 ## ###########################################################################
 tic = time.perf_counter()
 
@@ -37,14 +37,16 @@ sound.read(channel=0, chunk=[round(t1*fs), round(t2*fs)])
 
 # Calculates  spectrogram
 Spectro = Spectrogram(frame, window_type, nfft, step, fs, unit='samp')
-Spectro.compute(sound, dB=True, dask=True)
+Spectro.compute(sound, dB=True)
 
 toc = time.perf_counter()
 print(f"Executed in {toc - tic:0.4f} seconds")
 
-# # Plot
-# graph = GrapherFactory('SoundPlotter', title='Recording', frequency_max=1000)
-# graph.add_data(sound)
-# graph.add_data(Spectro)
-# graph.colormap = 'jet'
-# graph.show()
+# Plot
+graph = GrapherFactory('SoundPlotter', title='Recording', frequency_max=3000)
+graph.add_data(sound)
+graph.add_data(Spectro)
+graph.colormap = 'viridis'
+graph.show()
+plt.show()
+print('s')
--- tests/old_tests/tes_Raick.py
+++ tests/old_tests/tes_Raick.py
@@ -5,6 +5,9 @@ from ecosound.core.tools import filename_to_datetime
 #annot = Annotation()
 #annot.from_raven(root_dir)
 
+filename = r'C:\Users\xavier.mouy\Desktop\20220819T183000_2614231342965513_2.0dB_4.4V_ver2.00.wav'
+date = filename_to_datetime(filename)
+
 # print('d')
 #
 # file =r'C:\Users\xavie\Desktop\WHOI_fish\data_training_and_evaluation\20220711T174119.wav'
@@ -20,7 +23,7 @@ from ecosound.core.tools import filename_to_datetime
 
 
 
-annot_file = r'C:\Users\xavier.mouy\Desktop\raick\New folder'
-annot = Annotation()
-annot.from_raven(annot_file, class_header='Annotation')
+# annot_file = r'C:\Users\xavier.mouy\Desktop\raick\New folder'
+# annot = Annotation()
+# annot.from_raven(annot_file, class_header='Annotation')
 print('s')
